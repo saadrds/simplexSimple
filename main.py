04ad58a5ae38647df899384033aecb226da1acf0
safaeelhimer <safae.elhimer24@gmail.com>
2021-07-03 13:06:29 +0100

    pivot

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 def is_table_positive(table):
     print("table : ", table)
     for element in table:
-        if element < 0:
+        if element > 0:
             return 1
     return 0
 
@@ -25,10 +25,13 @@ def max_simplex(cout_z, constraint, b):
     grande_matrice += [couts_reduits]
     print(grande_matrice)
 
-    # starting the simplex iteration
-    while is_table_positive(grande_matrice[nb_constraint]):  # tanque les couts réduits positifs
+    for i in range(nb_constraint):
+        grande_matrice[i] += base_depart[i]
+    grande_matrice += [couts_reduits]
 
-        entrant_index = grande_matrice[nb_constraint].index(min(couts_reduits))  # cherchant la variable entrante
+    # starting the simplex iteration
+    while is_table_positive(grande_matrice[nb_constraint]):  # tanque les couts réduits negative
+        entrant_index = grande_matrice[nb_constraint].index(max(couts_reduits))  # cherchant la variable entrante
         sortant_index = -1
         # cherchant la variable sortante
         min_sortant = -1
@@ -52,7 +55,28 @@ def max_simplex(cout_z, constraint, b):
                     sortant_index = i  # variable sortante toruvée
 
         base_variable[sortant_index] = entrant_index  # changeons les variablede base
-        return grande_matrice[sortant_index][entrant_index]
+        pivot = grande_matrice[sortant_index][entrant_index]
+        print("Le pivot ", grande_matrice[sortant_index][entrant_index])
 
 
-print(max_simplex([-15, -14], [[9, 7], [1, 1]], [100, 12]))
+        for i in range(len(grande_matrice)):
+            if i != entrant_index:
+                grande_matrice[i][entrant_index] = 0
+
+        for i in range(len(grande_matrice[0])):
+            grande_matrice[sortant_index][i] = grande_matrice[sortant_index][i] / pivot
+
+        for i in range(len(grande_matrice)):
+            if i != sortant_index:
+                for j in range(len(grande_matrice[0])):
+                    if j != entrant_index:
+                        grande_matrice[i][j] = ((grande_matrice[i][j] * pivot) - (grande_matrice[sortant_index][j] *
+                                                                                  grande_matrice[i][entrant_index])) \
+                                               / pivot
+
+        return "resuu", grande_matrice
+
+
+#Print(max_simplex([-15, -14], [[9, 7], [1, 1]], [100, 12]))
+
+print(max_simplex([7, 9, 18, 17], [[2, 4, 5, 7], [1, 1, 2, 2], [1, 2, 3, 3]], [42, 17, 24]))
